5753b648d09e8872ec80149c36a89ac96531075f
galle <galle@DESKTOP-MFD5V2L>
2015-11-03 09:26:43 +0000

    Ejercicio terminado

---
+++ src/bol7/pkg1/Bol71.java
@@ -0,0 +1,18 @@
+package bol7.pkg1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author galle
+ */
+public class Bol71 {
+    public static void main(String[] args) {
+        int numero;
+        Positivonegativo obj1= new Positivonegativo();
+            numero = obj1.pedirNumero();
+            obj1.comparar(numero);
+            
+    }
+    
+}
